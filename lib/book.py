26801d4474287c5985f227f1339e651ffcfe0dc0
hillary-wangui <hillary.maina@student.moringaschool.com>
2024-01-09 04:37:57 +0300

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if not isinstance (page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    page_count = property(get_page_count, set_page_count)
+    
+
+
+book = Book("And Then There Were None", 272)
+
+book.turn_page()
+print(book.page_count)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self._condition = "New"
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if not isinstance (size, int):
+            print("size must be an integer")
+        else:
+            self._size = size
+    size = property(get_size, set_size)  
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+    def condition(self):
+        return self._condition
+
+
+shoe = Shoe("Adidas", 9)
+
+shoe.cobble()
+print(shoe.condition())
+print(shoe.size)
